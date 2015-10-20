cc4d6f10fee43ce81c2a8b1fffde96c883e288f3
Takuto Wada <takuto.wada@gmail.com>
2015-10-21 01:43:20 +0900

    docs(LICENSE): add license

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * power-assert-renderers.js - Power Assert output renderers
+ *
+ * https://github.com/twada/power-assert-renderers
+ *
+ * Copyright (c) 2015 Takuto Wada
+ * Licensed under the MIT license.
+ *   https://github.com/twada/power-assert-renderers/blob/master/MIT-LICENSE.txt
+ */
+
 'use strict';
 
 module.exports = {
